9b05de254590f113ae8b6d9d51b836104001bb04
Lê Nguyên Thi <thi.ln@3si.com.vn>
2020-05-14 16:18:50 +0700

    create

---
+++ LiteCommerce.Admin/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiteCommerce.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
